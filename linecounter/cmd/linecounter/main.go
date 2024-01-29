01dcd3a5948b63bd361b3fdd7d5cd8aed5876389
Lorenzo Drumond <lorenzo.drumond@neomediaworld.com>
2024-01-29 16:21:59 +0000

    Added error handling

--- linecounter/cmd/linecounter/main.go
+++ linecounter/cmd/linecounter/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"linecounter"
 )
 
 func main() {
-	count := linecounter.Main()
-	fmt.Println("---------------\nNumber of lines:", count)
+	linecounter.Main()
 }
--- linecounter/linecounter.go
+++ linecounter/linecounter.go
@@ -2,6 +2,8 @@ package linecounter
 
 import (
 	"bufio"
+	"errors"
+	"fmt"
 	"io"
 	"os"
 )
@@ -11,25 +13,42 @@ type counter struct {
 	output io.Writer
 }
 
-type option func(*counter)
+type option func(*counter) error
 
 func WithInput(input io.Reader) option {
-	return func(c *counter) {
+	return func(c *counter) error {
+		if input == nil {
+			return errors.New("nil input reader")
+		}
 		c.input = input
+		return nil
 	}
 }
 
-func NewCounter(opts ...option) counter {
+func WithOutput(output io.Writer) option {
+	return func(c *counter) error {
+		if output == nil {
+			return errors.New("nil output reader")
+		}
+		c.output = output
+		return nil
+	}
+}
+
+func NewCounter(opts ...option) (counter, error) {
 	c := counter{
 		input:  os.Stdin,
 		output: os.Stdout,
 	}
 
 	for _, opt := range opts {
-		opt(&c)
+		err := opt(&c)
+		if err != nil {
+			return counter{}, err
+		}
 	}
 
-	return c
+	return c, nil
 }
 
 func (c counter) Count() int {
@@ -43,6 +62,8 @@ func (c counter) Count() int {
 	return lines
 }
 
-func Main() int {
-	return NewCounter().Count()
+func Main() {
+	c, _ := NewCounter()
+	count := c.Count()
+	fmt.Println("---------------\nNumber of lines:", count)
 }
