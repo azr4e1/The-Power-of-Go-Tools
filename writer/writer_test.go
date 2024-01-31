a08642f29b61c3ae2b38d25dc8b2bad274c06234
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-01-31 21:49:20 +0000

    chore: commit save point

--- writer/writer_test.go
+++ writer/writer_test.go
@@ -10,11 +10,38 @@ import (
 
 func TestWriteToFile_WritesGivenDataToFile(t *testing.T) {
 	t.Parallel()
-	path := "testdata/write_test.txt"
-	if _, err := os.Stat(path); err == nil {
-		t.Fatalf("test artifact not cleaned up: %q", path)
+	path := t.TempDir() + "/write_test.txt"
+	want := []byte{1, 2, 3}
+	err := writer.WriteToFile(path, want)
+	if err != nil {
+		t.Fatal(err)
 	}
-	defer os.Remove(path)
+	got, err := os.ReadFile(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !cmp.Equal(want, got) {
+		t.Fatal(cmp.Diff(want, got))
+	}
+}
+
+func TestWriteToFile_ReturnsErrorForUnwritableFile(t *testing.T) {
+	t.Parallel()
+	path := "bogusdir/write_text.txt"
+	err := writer.WriteToFile(path, []byte{})
+	if err == nil {
+		t.Fatal("want error when file is not writable")
+	}
+}
+
+func TestWriteToFile_ClobbersGivenDataToFile(t *testing.T) {
+	t.Parallel()
+	path := t.TempDir() + "/write_test.txt"
+	prev_data := []byte{3, 2, 1}
+	err1 := writer.WriteToFile(path, prev_data)
+	if err1 != nil {
+		t.Fatal(err1)
+	}
 	want := []byte{1, 2, 3}
 	err := writer.WriteToFile(path, want)
 	if err != nil {
