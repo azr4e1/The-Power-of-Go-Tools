fd045c42f5c5ab3b4a57617f6b77080ade225274
Lorenzo Drumond <drumondlorenzo@posteo.net>
2024-02-05 19:07:35 +0000

    chore: commit save point

--- writer/writer.go
+++ writer/writer.go
@@ -1,10 +1,62 @@
 package writer
 
 import (
+	"errors"
+	"flag"
+	"fmt"
 	"os"
 )
 
 func WriteToFile(path string, data []byte) error {
-	err := os.WriteFile(path, data, 0644)
+	err := os.WriteFile(path, data, 0o600)
+	if err != nil {
+		return err
+	}
+	err = os.Chmod(path, 0o600)
 	return err
+}
+
+func BuildByteSlice(size int) ([]byte, error) {
+	if size < 0 {
+		return []byte{}, errors.New("size must be positive")
+	}
+	seq := []byte{}
+	for i := 0; i < size; i++ {
+		seq = append(seq, 0x00)
+	}
+	return seq, nil
+}
+
+func Main() int {
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [-size] [files...]\n",
+			os.Args[0])
+		fmt.Println("Creates a file with specified name and size")
+		fmt.Println("Flags:")
+		flag.PrintDefaults()
+	}
+	size := flag.Int("size", 0, "Create file of specified size")
+	flag.Parse()
+
+	content, err := BuildByteSlice(*size)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+
+	args := flag.Args()
+	if len(args) < 1 && *size != 0 {
+		fmt.Fprintln(os.Stderr, "you need to specify path to write to")
+		return 3
+	} else if len(args) < 1 {
+		return 0
+	}
+
+	err = WriteToFile(flag.Args()[0], content)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return 2
+	}
+
+	return 0
 }
--- writer/writer_test.go
+++ writer/writer_test.go
@@ -25,6 +25,28 @@ func TestWriteToFile_WritesGivenDataToFile(t *testing.T) {
 	}
 }
 
+func TestWriteToFile_GivesTheAppropriatePermissions(t *testing.T) {
+	t.Parallel()
+	path := t.TempDir() + "/perm_test.txt"
+	err := os.WriteFile(path, []byte{}, 0o644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = writer.WriteToFile(path, []byte{1, 2, 3})
+	if err != nil {
+		t.Fatal(err)
+	}
+	stat, err := os.Stat(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	perm := stat.Mode().Perm()
+	if perm != 0o600 {
+		t.Errorf("want file mode 0o600, got 0o%o", perm)
+	}
+}
+
 func TestWriteToFile_ReturnsErrorForUnwritableFile(t *testing.T) {
 	t.Parallel()
 	path := "bogusdir/write_text.txt"
@@ -38,12 +60,12 @@ func TestWriteToFile_ClobbersGivenDataToFile(t *testing.T) {
 	t.Parallel()
 	path := t.TempDir() + "/write_test.txt"
 	prev_data := []byte{3, 2, 1}
-	err1 := writer.WriteToFile(path, prev_data)
-	if err1 != nil {
-		t.Fatal(err1)
+	err := writer.WriteToFile(path, prev_data)
+	if err != nil {
+		t.Fatal(err)
 	}
 	want := []byte{1, 2, 3}
-	err := writer.WriteToFile(path, want)
+	err = writer.WriteToFile(path, want)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -53,5 +75,29 @@ func TestWriteToFile_ClobbersGivenDataToFile(t *testing.T) {
 	}
 	if !cmp.Equal(want, got) {
 		t.Fatal(cmp.Diff(want, got))
+	}
+}
+
+func TestBuildByteSlice_CreatesASliceOfBytesWithSpecifiedLength(t *testing.T) {
+	t.Parallel()
+	want := []byte{0, 0, 0, 0, 0}
+	got, err := writer.BuildByteSlice(5)
+	if err != nil {
+		t.Fatal("not expected error")
+	}
+	if len(got) != len(want) {
+		t.Errorf("got lenght %d, expected length %d", len(got), len(want))
+	}
+	if !cmp.Equal(want, got) {
+		t.Fatal(cmp.Diff(want, got))
+	}
+}
+
+func TestBuildByteSlice_Fails(t *testing.T) {
+	t.Parallel()
+	size := -10
+	_, err := writer.BuildByteSlice(size)
+	if err == nil {
+		t.Fatal("Expected error, got nil")
 	}
 }
