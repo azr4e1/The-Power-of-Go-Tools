8c325a43da4b1f7f447d7a991cafd5bad2f50d07
Lorenzo Drumond <lorenzo.drumond@neomediaworld.com>
2024-02-13 12:56:33 +0000

    chore: commit save point

---
+++ howlong/go.mod
@@ -0,0 +1,3 @@
+module howlong
+
+go 1.21.7
---
+++ howlong/howlong.go
@@ -0,0 +1,43 @@
+package howlong
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func TimeIt(command []string) (int64, error) {
+	currentTime := time.Now()
+	_, err := exec.Command(command[0], command[1:]...).CombinedOutput()
+	if err != nil {
+		return 0, err
+	}
+	finalTime := time.Now()
+
+	duration := finalTime.Sub(currentTime).Milliseconds()
+
+	return duration, nil
+}
+
+func Main() int {
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [command]\n", os.Args[0])
+		fmt.Println("Measure time taken by command")
+	}
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		flag.Usage()
+		return 0
+	}
+	duration, err := TimeIt(args)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "an error occurred")
+		fmt.Fprintln(os.Stderr, err)
+		return 1
+	}
+	fmt.Printf("This command took: %dms\n", duration)
+	return 0
+}
